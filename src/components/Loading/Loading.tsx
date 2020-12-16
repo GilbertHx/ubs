d56e114e6da419767335fc40c7168efbfe7764ff
Habimana Gilbert <habilbert@gmail.com>
2020-12-16 19:09:19 +0100

    Loading ...

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,25 @@
+import { motion } from "framer-motion";
 import React from "react";
 import './Loading.scss';
 
 const Loading: React.FC = () => {
   return (
         <div className="loader-wrapper">
-            <span className="loader"><span className="loader-inner"></span></span>
+            <motion.span 
+            className="loader"
+            animate={{
+              scale: [1, 1.5, 1.5, 1, 1],
+              rotate: [0, 180, 180, 360, 360],
+              borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+            }}
+            >
+              <motion.span 
+              animate={{
+                height: ['0%', '30%', '60%', '90%', '100%'],
+              }}
+              className="loader-inner"
+              ></motion.span>
+            </motion.span>
         </div>
         
   );
